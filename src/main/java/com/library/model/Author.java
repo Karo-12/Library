a3d2d9416b1655c69b922f9ef75ecb7e0ffc3717
Karolina <karolinaskawinska12@gmail.com>
2023-06-22 12:51:43 +0200

    Add hibernate annotations to entities. Closes #15

--- src/main/java/com/library/model/Author.java
+++ src/main/java/com/library/model/Author.java
@@ -1,18 +1,28 @@
 package com.library.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "author")
 public class Author {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "author_id")
     private UUID id;
+    @Column(name = "name", nullable = false, length = 15)
     private String name;
+    @Column(name = "surname", nullable = false, length = 15)
     private String surname;
+    @ManyToMany(mappedBy = "authors")
     private List<Book> releasedBooks;
 
 }
--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -1,7 +1,9 @@
 package com.library.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,13 +11,36 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "book_id")
     private UUID id;
+    @Column(name= "title", nullable = false, length = 32)
     private String title;
-    private List<Author> authors;
+    @Column(name = "isbn", nullable = false, length = 15)
     private String isbn;
+    @Column(name = "release_date", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
+    @Column(name = "description")
     private String description;
+    @Column(name = "status")
     private BookStatus status;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "books_authors",
+            joinColumns = @JoinColumn(name="book_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id")
+    )
+    private List<Author> authors;
+
+    @PrePersist
+    void setStatus(){
+        status = BookStatus.AVAILABLE;
+    }
 }
